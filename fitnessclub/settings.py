412e602fc9831343ec41ca6072bcd0fe8372a7db
ANMOL <lucky9012>
2022-01-27 21:43:11 +0545

    third

--- fitnessclub/settings.py
+++ fitnessclub/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["fitnessclub24.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["myfitnessclub24.herokuapp.com","localhost"]
 
 
 # Application definition
